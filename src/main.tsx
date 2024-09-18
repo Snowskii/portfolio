9c106bf51556103d38ab3828b6a1fdeb6124c041
Snowskii <kiralmichal1@gmail.com>
2024-09-18 15:10:04 +0200

    Change the router so it works on github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import HomeScreen from "./screens/HomeScreen.tsx";
 import BangHomeScreen from "./screens/BangTheDuelGenerator/BangHomeScreen.tsx";
 import GameGeneratorScreen from "./screens/BangTheDuelGenerator/GameGeneratorScreen.tsx";
 
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomeScreen />,
-  },
-  {
-    path: "/bangtheduelgenerator",
-    element: <BangHomeScreen />,
-  },
-  {
-    path: "/bangtheduelgenerator/game-generator",
-    element: <GameGeneratorScreen />,
-  },
-]);
-
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <HashRouter> {/* Change this line */}
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/bangtheduelgenerator" element={<BangHomeScreen />} />
+        <Route path="/bangtheduelgenerator/game-generator" element={<GameGeneratorScreen />} />
+      </Routes>
+    </HashRouter> {/* Change this line */}
   </StrictMode>
 );
