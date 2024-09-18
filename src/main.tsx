4655742fa916d39d15ac2cca5bbc4a3254c6b96f
Snowskii <kiralmichal1@gmail.com>
2024-09-18 14:19:39 +0200

    create router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import HomeScreen from "./screens/homeScreen.tsx";
+import BangHomeScreen from "./screens/BangTheDuelGenerator/BangHomeScreen.tsx";
+import GameGeneratorScreen from "./screens/BangTheDuelGenerator/GameGeneratorScreen.tsx";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeScreen />,
+  },
+  {
+    path: "/bangtheduelgenerator",
+    element: <BangHomeScreen />,
+  },
+  {
+    path: "/bangtheduelgenerator/game-generator",
+    element: <GameGeneratorScreen />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>Hello!</div>
+    <RouterProvider router={router} />
   </StrictMode>
 );
