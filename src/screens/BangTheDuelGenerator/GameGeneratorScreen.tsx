3f74275ae8a3fda6ca1498d362ccbeea59ebc5e8
Snowskii <kiralmichal1@gmail.com>
2024-09-18 14:19:54 +0200

    Add screens

---
+++ src/screens/BangTheDuelGenerator/GameGeneratorScreen.tsx
@@ -0,0 +1,7 @@
+import GameGenerator from "../../applications/bangtheduelgenerator/components/gameGenerator/game-generator";
+
+const BangHomeScreen: React.FC = () => {
+  return <GameGenerator></GameGenerator>;
+};
+
+export default BangHomeScreen;
